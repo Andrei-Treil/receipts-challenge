d34506888170e95271613b5cb51fa044bc1973d7
Andrei-Treil <andrei.treil@gmail.com>
2024-06-18 17:37:32 -0400

    Added negative test cases

--- backend/test_server.py
+++ backend/test_server.py
@@ -25,3 +25,15 @@ def testExamples(receipt):
     response = client.get(f"receipts/{response.json['id']}/points")
     assert response.status_code == 200
     assert response.json["points"] == receipt["points"]
+
+@pytest.mark.parametrize("id", ['1','2','3'])
+def testBadId(id):
+    response = client.get(f"receipts/{id}/points")
+    assert response.status_code == 404
+
+@pytest.mark.parametrize("json",[{ "badKey": 1010},
+                        {"badderKey": True,"worseKey": ""}])
+
+def testBadJson(json):
+    response = client.post("receipts/process", json=json)
+    assert response.status_code == 400
